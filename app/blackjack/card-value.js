066494b6b7d190c7d362fbe41e1e48f433525f30
Derrick Bray <derrickbray@yahoo.com>
2016-10-10 16:38:02 -0500

    card value

--- app/blackjack/card-value.js
+++ app/blackjack/card-value.js
@@ -1,3 +1,17 @@
 export default function (cardName) {
-
+  if (cardName === 'ace') {
+    return 11;
+  }
+  if (cardName === 'king') {
+    return 10;
+  }
+  if (cardName === 'queen') {
+    return 10;
+  }
+  if (cardName === 'jack') {
+    return 10;
+  }
+  else {
+    return parseInt(cardName);
+  }
 }
