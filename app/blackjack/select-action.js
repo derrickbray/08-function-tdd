6969a2518727811a08626e2c89ffb57233ba8d29
Derrick Bray <derrickbray@yahoo.com>
2016-10-10 17:59:22 -0500

    blackjack

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -2,5 +2,12 @@ import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
 export default function (card1, card2) {
+  if (shouldHit(card1, card2)) {
+    return 'hit';
+  }
+  if (shouldSplit(card1, card2)) {
+    return 'split';
+  }
 
+  return 'stay';
 }
