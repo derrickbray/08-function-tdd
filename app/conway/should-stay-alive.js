abbfe4987229425364a18e75b9d459a1f68034b9
Derrick Bray <derrickbray@yahoo.com>
2016-10-12 13:54:47 -0500

    staying alive

--- app/conway/should-stay-alive.js
+++ app/conway/should-stay-alive.js
@@ -1,18 +1,12 @@
 import aliveNeighbors from './alive-neighbors';
-const alive = 0;
+
 export default function shouldStayAlive(neighbors) {
-    for (let i = 0; i < aliveNeighbors.length; i++) {
-        if (aliveNeighbors[i] > 3) {
-            return false;
-        }
-        if (aliveNeighbors[i] < 2) {
-            return false;
-        } else {
-            return true;
-        }
-
-    }
-}
-
-return aliveNeighbors;
+  if (aliveNeighbors(neighbors) > 3) {
+    return false;
+  }
+  if (aliveNeighbors(neighbors) < 2) {
+    return false;
+  } else {
+    return true;
+  }
 }
