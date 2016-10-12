96162e53017706cd18e35749d3992c6a61641d2d
Derrick Bray <derrickbray@yahoo.com>
2016-10-12 14:09:35 -0500

    why wont you die

--- app/conway/should-revive.js
+++ app/conway/should-revive.js
@@ -1,5 +1,12 @@
 import aliveNeighbors from './alive-neighbors';
 
 export default function shouldStayAlive(neighbors) {
-
+  if (aliveNeighbors(neighbors) > 3) {
+    return false;
+  }
+  if (aliveNeighbors(neighbors) < 2) {
+    return false;
+  } else {
+    return true;
+  }
 }
